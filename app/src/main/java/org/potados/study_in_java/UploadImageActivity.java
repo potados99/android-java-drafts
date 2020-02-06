b9a8d0b00337a7ff1f00c18af38719e3ebc276b2
potados99 <potados99@gmail.com>
2020-02-06 19:16:48 +0900

    Import 정리.

--- app/src/main/java/org/potados/study_in_java/UploadImageActivity.java
+++ app/src/main/java/org/potados/study_in_java/UploadImageActivity.java
@@ -3,10 +3,8 @@ package org.potados.study_in_java;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
