2177b7e04a694a3743c1b03d032f9884ca8fcde1
potados99 <potados99@gmail.com>
2020-02-06 20:54:30 +0900

    OnClickListener 예제 추가.

--- app/src/main/java/org/potados/study_in_java/MainActivity.java
+++ app/src/main/java/org/potados/study_in_java/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 import org.potados.study_in_java.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     MainViewModel vm = new MainViewModel();
 
     @Override
@@ -28,12 +28,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // startActivity(new Intent(MainActivity.this, UploadImageActivity.class));
 
+        // 첫 번째 방법.
+        binding.newActivityButton.setOnClickListener(new TestClickListener());
+
+        // 두 번째 방법.
         binding.newActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, UploadImageActivity.class));
+                Toast.makeText(v.getContext(), "Click!", Toast.LENGTH_SHORT).show();
             }
         });
+
+        // 2.5번째 방법.
+        // 구현해야 할 메소드가 하나일 때!!!!!!!!!!!! 람다식 사용 삽가능!!
+        binding.newActivityButton.setOnClickListener(v -> Toast.makeText(v.getContext(), "Click!", Toast.LENGTH_SHORT).show());
+
+        // 세 번째 방법.
+        binding.newActivityButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(v.getContext(), "Click!", Toast.LENGTH_SHORT).show();
     }
 }
