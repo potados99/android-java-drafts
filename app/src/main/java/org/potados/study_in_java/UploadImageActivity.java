2007800638769fc092ed6616a1a8163bee19b6b9
ryujisuda <cherryhee_@naver.com>
2020-02-06 21:16:06 +0900

    지수

--- app/src/main/java/org/potados/study_in_java/UploadImageActivity.java
+++ app/src/main/java/org/potados/study_in_java/UploadImageActivity.java
@@ -41,7 +41,7 @@ import retrofit2.Response;
  *  |
  *  |   인텐트 처리 완료 이벤트
  *  |       -> onActivityResult
- *  |           -> pickiT.getPath()
+ *  |           -> pickiT.getPath() //URI를 통해 실제 경로를 알려줌. 쭉 비동기로 진행됨.
  *  |
  *  v   ...(PickiT의 파일 처리)...
  *  |
@@ -74,6 +74,14 @@ import retrofit2.Response;
  *  retrofit2 라이브러리를 사용하여 파일을 업로드하기 위해서는 해당 파일의 절대경로가 필요함.
  *
  *  안드로이드에서 uri를 통해 절대경로를 알아내는 것은 매우 복잡함. 안드로이드 버전에 따라(킷캣 전/후, 오레오 전/후),
+ *  로컬파일이 아닌 경우 파일을 복사해서 다른 경로에다 놔야한다. 다른 경로는 안드로이드 시스템이 권장하는 캐시 디렉토리.
+ *  URI가 가리키는 곳에 파일이 있다. 바로 가져올 수 없다.. 자바에 inputstream이 있는데 URI를 가지고 URI에 해당하는 파일의 inputstream은 열 수 있다.
+ *  inputstream?->꺼내서 읽는 흐름.. 입력용 파일로부터 읽는거. 인풋스트림을 열라면 파일을 지정해줘야한다. 파일 대신 URI로 지정해줄 수 있따.
+ *  파일의 실제 경로를 몰라도 URI를 알고있으면 URI를 가리키는 파일의 인풋스트림을 열 수 있다. 인풋스트림을 열면 파일의 내용이 있는데 하나씩 빼와서 캐시 디렉토리에 복사한다.
+ *  복사가 끝나면 캐시디렉토리에 있는 파일은 절대 경로를 바로 가져올 수 있다. 이걸 레트로핏에 넘겨준다.
+ *
+ *  URIHELPER가 안됐던 이유..?
+ *
  *  그리고 파일의 위치에 따라, 파일 제공자의 종류에 따라 uri의 형식이 매우 달라짐.
  *  이에 대응하기 위해 가능한 한 모든 경우를 커버하는 UriHelper 클래스를 작성하였으나, 결국 포기함.
  *
@@ -84,10 +92,11 @@ import retrofit2.Response;
  *  2. 안드로이드 8(또는 이상)에서의 파일 다루기
  *
  *  안드로이드 8 이상에서, 어떤 파일은 uri를 통해 절대경로를 바로 추출하지 못함.
- *  예를 들어, uri가 content://com.android.providers.downloads.documents/document/37 와 같이 주어질 때,
+ *  예를 들어, uri가 content://com.android.providers.downloads.documents/document/37 와 같이 주어질 때, (파일명 안보여)
  *  이를 변형한 content://downloads/public_downloads/37 형태의 uri를 사용해 contentProvider에 조회를 요청하여
  *  _data 컬럼을 가져오는 방법으로 절대경로를 알아내야 하는데, 안드로이드 8에서는 이 방법을 사용할 수 없음
  *  (유효하지 않은 uri로 식별됨).
+ *
  *
  *  결국 해당 uri가 가리키는 파일의 복사본을 캐시 디렉토리에 새로 만들어 해당 캐시 파일의 경로를 사용해야 함.
  *  이 접근은 안드로이드 8 이상에서 파일 경로를 확보하기 위한 유일한 방법인 것으로 보임.
@@ -150,6 +159,7 @@ public class UploadImageActivity extends AppCompatActivity implements PickiTCall
 
         // PickiT 초기화
         pickiT = new PickiT(this, this);
+        //리스너랑 콜백이랑 같다.
     }
 
     /**
@@ -168,6 +178,7 @@ public class UploadImageActivity extends AppCompatActivity implements PickiTCall
             pickiT.getPath(data.getData(), Build.VERSION.SDK_INT);
         }
     }
+    //onActivityResult가 메인쓰레드에서 실행된다.
 
     /**
      * 권한 요청 처리 후에 호출됨
